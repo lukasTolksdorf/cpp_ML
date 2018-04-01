2ad90e640be26bb30538c49ac981a15f8e77f9b2
Lukas Tolksdorf <Lukas.Tolksdorf@tum.de>
2018-04-01 19:31:38 +0200

    typo

--- test/matrix/testdimension.cpp
+++ test/matrix/testdimension.cpp
@@ -6,5 +6,5 @@ TEST(DimensionTest, OperationEQUALTO){
   EXPECT_TRUE(matr::Dimension(5,5) == matr::Dimension(5,5));
   EXPECT_FALSE(matr::Dimension(4,5) == matr::Dimension(5,4));
   EXPECT_FALSE(matr::Dimension(1,2) == matr::Dimension(1,1));
-  EXPEXT_FALSE(matr::Dimension(7,7) == matr::Dimension(6,7));
+  EXPECT_FALSE(matr::Dimension(7,7) == matr::Dimension(6,7));
 }
